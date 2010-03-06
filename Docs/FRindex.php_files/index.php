14aa68f5b35f0d0257abfe70941832e30ab55ece
alabamacajun <alabamacajun@7c74f4f4-39d4-4077-a54d-10deffb394f4>
2010-03-06 14:46:56 +0000

    git-svn-id: https://tpb3dtg.svn.sourceforge.net/svnroot/tpb3dtg@4 7c74f4f4-39d4-4077-a54d-10deffb394f4

--- Docs/FRindex.php_files/index.php
+++
@@ -1,9 +0,0 @@
-/* generated javascript */
-var skin = 'monobook';
-var stylepath = '/tpb3dwiki/skins';
-
-/* MediaWiki:Common.js */
-/* Any JavaScript here will be loaded for all users on every page load. */
-
-/* MediaWiki:Monobook.js (deprecated; migrate to Common.js!) */
-/* Deprecated; use [[MediaWiki:common.js]] */
