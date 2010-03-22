ad6abd98b6140fa533951a4371266c34a69710df
alabamacajun <alabamacajun@7c74f4f4-39d4-4077-a54d-10deffb394f4>
2010-03-22 04:15:14 +0000

    Renaming objecttree.h to ObjectTree.h
    git-svn-id: https://tpb3dtg.svn.sourceforge.net/svnroot/tpb3dtg@18 7c74f4f4-39d4-4077-a54d-10deffb394f4

---
+++ Resources.h
@@ -0,0 +1,9 @@
+#pragma once
+// This project is covered under the GPL license, see HighRiseDev.h
+// #DEFINES
+
+#define HR_VERSION "0.0.1 PREALPHA" //ULTRABETA" we're not yet Alpha
+
+// Other resources that need to be OS or user mutable (subject to changes by the user or users OS)
+
+// Inverstigate internationalization
