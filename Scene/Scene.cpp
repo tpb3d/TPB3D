3d544b2963f19ae652cf1ef307724abc72ef6a87
Duckblaster <duckblaster7090@gmail.com>
2011-06-25 10:56:57 -1000

    fix formating and update gitignore

--- Scene/Scene.cpp
+++ Scene/Scene.cpp
@@ -30,67 +30,67 @@
 //using namespace Gfx;
 Scene::Scene ()
 {
-   mpTerrain = NULL;
-   mpSky = new SkyBowl();
-   mpSky->Init( 800 );
-   mpDefaultStrategy = new SelectDelegate("Default", this);
-   mpStrategy = mpDefaultStrategy;
+	mpTerrain = NULL;
+	mpSky = new SkyBowl();
+	mpSky->Init( 800 );
+	mpDefaultStrategy = new SelectDelegate("Default", this);
+	mpStrategy = mpDefaultStrategy;
 }
 
 Scene::~Scene ()
 {
-   delete mpSky;
-   if (! (mpStrategy == NULL || mpStrategy == mpDefaultStrategy))
-   {
-      delete mpStrategy;
-   }
-   delete mpDefaultStrategy;
+	delete mpSky;
+	if (! (mpStrategy == NULL || mpStrategy == mpDefaultStrategy))
+	{
+		delete mpStrategy;
+	}
+	delete mpDefaultStrategy;
 }
 
 void Scene::AddPark (Park* pPark)
 {
-   mParks.push_back (pPark);   // Multiple parks are possible with piping
+	mParks.push_back (pPark);   // Multiple parks are possible with piping
 }
 
 void Scene::SetTerrain (Terrain* pTerrain)
 {
-   if (mpTerrain != NULL)
-   {
-      delete mpTerrain;
-   }
-   mpTerrain = pTerrain;
+	if (mpTerrain != NULL)
+	{
+		delete mpTerrain;
+	}
+	mpTerrain = pTerrain;
 }
 
 
 bool Scene::SelectTool (int toolID)
 {
-   bool bResult = false;
-   DelegateBase* pOldStrategy = mpStrategy;
-   switch (toolID)
-   {
-   case HR_PlaceBuilding:
-      mpStrategy = BuildStrategyFactory::MakeStrategy(0, this); // this does not really work
-      mpStrategy->Draw();
-      bResult = true;
-      break;
-   case HR_PlacePath:
-      mpStrategy = BuildStrategyFactory::MakeStrategy(0, this); // this does not really work
-      mpStrategy->Draw();
-      bResult = true;
-      break;
-   }
-   if (bResult && ! (mpStrategy == NULL || mpStrategy == mpDefaultStrategy))
-   {
-      try
-      {
-         delete pOldStrategy;
-      }
-      catch (...)
-      {
-         std::cout << "Error deleting old BuildStrategy in Scene";
-      }
-   }
-   return bResult;
+	bool bResult = false;
+	DelegateBase* pOldStrategy = mpStrategy;
+	switch (toolID)
+	{
+	case HR_PlaceBuilding:
+		mpStrategy = BuildStrategyFactory::MakeStrategy(0, this); // this does not really work
+		mpStrategy->Draw();
+		bResult = true;
+		break;
+	case HR_PlacePath:
+		mpStrategy = BuildStrategyFactory::MakeStrategy(0, this); // this does not really work
+		mpStrategy->Draw();
+		bResult = true;
+		break;
+	}
+	if (bResult && ! (mpStrategy == NULL || mpStrategy == mpDefaultStrategy))
+	{
+		try
+		{
+			delete pOldStrategy;
+		}
+		catch (...)
+		{
+			std::cout << "Error deleting old BuildStrategy in Scene";
+		}
+	}
+	return bResult;
 }
 
 void Scene::Update (float dt, int timeOfDay)
@@ -99,127 +99,127 @@ void Scene::Update (float dt, int timeOfDay)
 
 void Scene::Draw ()
 {
-   std::vector<Park *>::iterator iPark;
-//glEnable(GL_FOG);
-   // Acutal
-   glEnable(GL_CULL_FACE);
-   glDisable(GL_LIGHTING);
-   glEnable (GL_COLOR_MATERIAL);
-   mpSky->Draw();
-   glEnable(GL_LIGHTING);
-   glDisable(GL_LIGHT0);
-   glEnable(GL_LIGHT1);
-   //glDisable (GL_COLOR_MATERIAL);
+	std::vector<Park *>::iterator iPark;
+	//glEnable(GL_FOG);
+	// Acutal
+	glEnable(GL_CULL_FACE);
+	glDisable(GL_LIGHTING);
+	glEnable (GL_COLOR_MATERIAL);
+	mpSky->Draw();
+	glEnable(GL_LIGHTING);
+	glDisable(GL_LIGHT0);
+	glEnable(GL_LIGHT1);
+	//glDisable (GL_COLOR_MATERIAL);
 	glEnable(GL_NORMALIZE);
-   mpTerrain->Draw();
-   if(mpTerrain->Water()!=NULL)	mpTerrain->Water()->Draw();
-   //glDisable(GL_LIGHT1);
+	mpTerrain->Draw();
+	if(mpTerrain->Water()!=NULL)	mpTerrain->Water()->Draw();
+	//glDisable(GL_LIGHT1);
 	glDisable(GL_CULL_FACE);
-   glDisable(GL_LIGHT1);
-   glEnable(GL_LIGHT0);
+	glDisable(GL_LIGHT1);
+	glEnable(GL_LIGHT0);
 
-   for (iPark = mParks.begin (); iPark != mParks.end (); iPark++)
-   {
-      Park* pPark = (*iPark);
-      pPark->Draw( );
-   }
+	for (iPark = mParks.begin (); iPark != mParks.end (); iPark++)
+	{
+		Park* pPark = (*iPark);
+		pPark->Draw( );
+	}
 	glDisable(GL_NORMALIZE);
 }
 
 void Scene::DrawSelectionTarget (int PathwayNo)
 {
-   bool bPathwaysOnly = (this->mpStrategy==NULL) ? false : true;
-   std::vector<Park *>::iterator iPark;
-   for (iPark = mParks.begin (); iPark != mParks.end (); iPark++)
-   {
-      if (PathwayNo==0)
-      {
-         (*iPark)->DrawSelectionTarget (bPathwaysOnly);
-      }
-      else
-      {
-      }
-   }
+	bool bPathwaysOnly = (this->mpStrategy==NULL) ? false : true;
+	std::vector<Park *>::iterator iPark;
+	for (iPark = mParks.begin (); iPark != mParks.end (); iPark++)
+	{
+		if (PathwayNo==0)
+		{
+			(*iPark)->DrawSelectionTarget (bPathwaysOnly);
+		}
+		else
+		{
+		}
+	}
 }
 
 void Scene::MoveGhostObject (Vector2f& point)
 {
-   Camera* pCam = Camera::GetInstance();
-   Vector3f vec = pCam->GetOGLPos (point);
-   Park* pPark = mParks[0];
-   pPark->GetGhostObject().Move (vec);// asp
+	Camera* pCam = Camera::GetInstance();
+	Vector3f vec = pCam->GetOGLPos (point);
+	Park* pPark = mParks[0];
+	pPark->GetGhostObject().Move (vec);// asp
 }
 
 bool Scene::OnFRCS ()
 {
-   return true;
+	return true;
 }
 
 bool Scene::OnTRCS ()
 {
-   return true;
+	return true;
 }
 
 bool Scene::OnPlacePathItem ()
 {
-   bool bResult = false;
-   DelegateBase* pOldStrategy = mpStrategy;
-   mpStrategy = new PlaceItemDelegate<Stall>("Generic Bench", this); //<CBench*>();
-   mpStrategy->Draw();
-   bResult = true;
+	bool bResult = false;
+	DelegateBase* pOldStrategy = mpStrategy;
+	mpStrategy = new PlaceItemDelegate<Stall>("Generic Bench", this); //<CBench*>();
+	mpStrategy->Draw();
+	bResult = true;
 
-   if (bResult && ! (pOldStrategy == NULL || pOldStrategy == mpDefaultStrategy))
-   {
-      try
-      {
-         delete pOldStrategy;
-      }
-      catch (...)
-      {
-         std::cout << "Error deleting old BuildStrategy in Scene";
-      }
-   }
-   return bResult;
+	if (bResult && ! (pOldStrategy == NULL || pOldStrategy == mpDefaultStrategy))
+	{
+		try
+		{
+			delete pOldStrategy;
+		}
+		catch (...)
+		{
+			std::cout << "Error deleting old BuildStrategy in Scene";
+		}
+	}
+	return bResult;
 }
 
 void Scene::HitDown( int hit, Vector2i& Scene )  // taking a mouse hit, send it through geometry to see what we hit
 {
-   if( mpStrategy ) //&& mpStrategy->Placing() )
-   {
-      mpStrategy->OnHitDown(hit);
-   }
-   else
-   {
-      // send the hit through a strategy delegate
-      //CoreBase* pFS = FindFloorSpace(hit);
-      //std::vector<Park *>::iterator iPark;
-      //for (iPark = mParks.begin (); iPark != mParks.end (); iPark++)
-      //{
-      //   Park* pPark = (*iPark);
-      //   Pathway* pPathway = pPark->FindPathway (hit);
-      //   if( pPathway )
-      //   {
-      //      int x = (int)(pPark->GetGhostObject().GetX() / 9);
-      //      std::cout << "Mouse on Pathway: " << pPathway->GetPathway() << " Pathway ID: " << hit << std::endl;
-      //      mpStrategy->BuildHere(pPark, x, pPathway->GetPathway());
-      //      break;
-      //   }
-      //   else
-      //   {
-      //      std::cout << "Mouse landed on an unresgister object (BUG)" << " Pathway ID: " << hit  << std::endl;
-      //   }
-      //}
-   }
+	if( mpStrategy ) //&& mpStrategy->Placing() )
+	{
+		mpStrategy->OnHitDown(hit);
+	}
+	else
+	{
+		// send the hit through a strategy delegate
+		//CoreBase* pFS = FindFloorSpace(hit);
+		//std::vector<Park *>::iterator iPark;
+		//for (iPark = mParks.begin (); iPark != mParks.end (); iPark++)
+		//{
+		//   Park* pPark = (*iPark);
+		//   Pathway* pPathway = pPark->FindPathway (hit);
+		//   if( pPathway )
+		//   {
+		//      int x = (int)(pPark->GetGhostObject().GetX() / 9);
+		//      std::cout << "Mouse on Pathway: " << pPathway->GetPathway() << " Pathway ID: " << hit << std::endl;
+		//      mpStrategy->BuildHere(pPark, x, pPathway->GetPathway());
+		//      break;
+		//   }
+		//   else
+		//   {
+		//      std::cout << "Mouse landed on an unresgister object (BUG)" << " Pathway ID: " << hit  << std::endl;
+		//   }
+		//}
+	}
 }
 
 void Scene::HitUp( int hit, Vector2i& Scene )  // taking a mouse hit, send it through geometry to see what we hit
 {
-   if( mpStrategy ) //&& mpStrategy->Placing() )
-   {
-      mpStrategy->OnHitUp(hit);
-   }
-   else
-   {
-      // send the hit through a strategy delegate
-   }
+	if( mpStrategy ) //&& mpStrategy->Placing() )
+	{
+		mpStrategy->OnHitUp(hit);
+	}
+	else
+	{
+		// send the hit through a strategy delegate
+	}
 }
--- TPB3DTG.cpp
+++ TPB3DTG.cpp
@@ -1,18 +1,18 @@
 /*   This file is part of Theme Park Builder 3D The Game.
- *
- *   Theme Park Builder 3D The Game is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+*
+*   Theme Park Builder 3D The Game is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
 
- *   Theme Park Builder 3D The Game is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with Theme Park Builder 3D The Game.  If not, see <http://www.gnu.org/licenses/>.
- */
+*   Theme Park Builder 3D The Game is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with Theme Park Builder 3D The Game.  If not, see <http://www.gnu.org/licenses/>.
+*/
 // Original code from Extreme Park and Highrise Developer
 // By AlabamaCajun, RFrank, crazy2be
 
@@ -38,75 +38,68 @@
 
 
 
-void debugprint()
-{
-   std::cout << "Program exited correctly. Creating trace...\n";
+void debugprint() {
+	std::cout << "Program exited correctly. Creating trace...\n";
 }
 
-int
-main ()
-{
+int main () {
 
-//////////////////////////////////////////
-//
-// Set up and display Splash Screen
-//
-  sf::Image SplashScreenImage;      // "Image" is a pixel array in memory
-  sf::Sprite SplashScreenSprite;    // "Sprite" is a displayable interface to "Image"
-  int SplashScreenLeft;            // left side of splash screen on desktop
-  int SplashScreenTop;             // top of splash screen on desktop
-  int SplashScreenWidth;           // width of splash screen (to be determined by inspecting the image after loading
-  int SplashScreenHeight;          // height of splash screen (to be determined by inspecting the image after loading
-  sf::Clock SplashScreenClock;      // timer to keep the splash screen displayed for a minumum amount of time
+	//////////////////////////////////////////
+	//
+	// Set up and display Splash Screen
+	//
+	sf::Image SplashScreenImage;      // "Image" is a pixel array in memory
+	sf::Sprite SplashScreenSprite;    // "Sprite" is a displayable interface to "Image"
+	int SplashScreenLeft;            // left side of splash screen on desktop
+	int SplashScreenTop;             // top of splash screen on desktop
+	int SplashScreenWidth;           // width of splash screen (to be determined by inspecting the image after loading
+	int SplashScreenHeight;          // height of splash screen (to be determined by inspecting the image after loading
+	sf::Clock SplashScreenClock;      // timer to keep the splash screen displayed for a minumum amount of time
 
-  if (!SplashScreenImage.LoadFromFile("data/splashscreen.png")) // attempt to load the splashscreen (note that the user can change this file
-  {
-    std::cout << "Error: Unable to load Splash Screen - file not found\n"; // Error...
-  }
+	if (!SplashScreenImage.LoadFromFile("data/splashscreen.png")) { // attempt to load the splashscreen (note that the user can change this file
+		std::cout << "Error: Unable to load Splash Screen - file not found\n"; // Error...
+	}
 
-  SplashScreenWidth = SplashScreenImage.GetWidth();
-  SplashScreenHeight = SplashScreenImage.GetHeight();
+	SplashScreenWidth = SplashScreenImage.GetWidth();
+	SplashScreenHeight = SplashScreenImage.GetHeight();
 
-  // set up a desktop window with render context, etc.
-  sf::RenderWindow SplashScreen(sf::VideoMode(SplashScreenWidth,SplashScreenHeight, 32), "", sf::Style::None);
+	// set up a desktop window with render context, etc.
+	sf::RenderWindow SplashScreen(sf::VideoMode(SplashScreenWidth,SplashScreenHeight, 32), "", sf::Style::None);
 
-  SplashScreenLeft = (sf::VideoMode::GetDesktopMode().Width - SplashScreenWidth)/2;     // determine left edge based on desktop and image sizes
-  SplashScreenTop  = (sf::VideoMode::GetDesktopMode().Height - SplashScreenHeight)/2;   // determine top edge based on desktop and image sizes
-  if (SplashScreenLeft < 0) SplashScreenLeft = 0;  // clamp value in case of big image on small desktop
-  if (SplashScreenTop  < 0) SplashScreenTop  = 0;  // clamp value in case of big image on small desktop
+	SplashScreenLeft = (sf::VideoMode::GetDesktopMode().Width - SplashScreenWidth)/2;     // determine left edge based on desktop and image sizes
+	SplashScreenTop  = (sf::VideoMode::GetDesktopMode().Height - SplashScreenHeight)/2;   // determine top edge based on desktop and image sizes
+	if (SplashScreenLeft < 0) SplashScreenLeft = 0;  // clamp value in case of big image on small desktop
+	if (SplashScreenTop  < 0) SplashScreenTop  = 0;  // clamp value in case of big image on small desktop
 
-  SplashScreen.SetPosition(SplashScreenLeft, SplashScreenTop);  // set the window position to be the center of the desktop
-  SplashScreenSprite.SetImage(SplashScreenImage);               // associate pixel array ("Image") with rendering class ("Sprite")
-  SplashScreen.Draw(SplashScreenSprite);                        // render the image to the splash screen window
-  SplashScreen.Display();                                       // we can finally draw it to the desktop!!
+	SplashScreen.SetPosition(SplashScreenLeft, SplashScreenTop);  // set the window position to be the center of the desktop
+	SplashScreenSprite.SetImage(SplashScreenImage);               // associate pixel array ("Image") with rendering class ("Sprite")
+	SplashScreen.Draw(SplashScreenSprite);                        // render the image to the splash screen window
+	SplashScreen.Display();                                       // we can finally draw it to the desktop!!
 
-  SplashScreenClock.Reset();      // begin timing from this point.
+	SplashScreenClock.Reset();      // begin timing from this point.
 
-//
-//  Splash Screen now displayed
-//
-//////////////////////////////////////////////
+	//
+	//  Splash Screen now displayed
+	//
+	//////////////////////////////////////////////
 
 
-//////////////////////////////////////////////
-//
-//  Set up background music
-//
-  sf::Music MusicBackGround;
+	//////////////////////////////////////////////
+	//
+	//  Set up background music
+	//
+	/*sf::Music MusicBackGround;
 
-  if (!MusicBackGround.OpenFromFile("data/Sound/04_horse_don_t_fly.ogg"))
-  {
-    std::cout << "Error: Unable to load background music file.\n";// Error...
-  }
-  else
-  {
-    MusicBackGround.Play();
-    MusicBackGround.SetLoop(true);
-  }
-//
-//  Background music will now loop as long as the program runs
-//
-//////////////////////////////////////////////////
+	if (!MusicBackGround.OpenFromFile("data/Sound/04_horse_don_t_fly.ogg")) {
+		std::cout << "Error: Unable to load background music file.\n";// Error...
+	} else {
+		MusicBackGround.Play();
+		MusicBackGround.SetLoop(true);
+	}*/
+	//
+	//  Background music will now loop as long as the program runs
+	//
+	//////////////////////////////////////////////////
 
 
 
@@ -114,119 +107,111 @@ main ()
 
 
 
-   atexit(debugprint);
-   Camera * cam = Camera::GetInstance ();
+	atexit(debugprint);
+	Camera * cam = Camera::GetInstance ();
 
-   cam->SetSceneSize (Vector2f(1280, 720));
-   cam->SetMaxFramerate (120);
-   cam->SetActive();
-   cam->InitGL();
+	cam->SetSceneSize (Vector2f(1280, 720));
+	cam->SetMaxFramerate (120);
+	cam->SetActive();
+	cam->InitGL();
 
-   Scene theScene;
-   Park thePark (1, 10, theScene);
-   theScene.AddPark (&thePark);
-   GameManager gm(theScene, thePark);
-   gm.InitialSetup();
-   PeepsAgent People( thePark ); // known Park, later this will be a Park list for mutiple Parks
-   sf::String Title( string("Alpha"));
+	Scene theScene;
+	Park thePark (1, 10, theScene);
+	theScene.AddPark (&thePark);
+	GameManager gm(theScene, thePark);
+	gm.InitialSetup();
+	PeepsAgent People( thePark ); // known Park, later this will be a Park list for mutiple Parks
+	sf::String Title( string("Alpha"));
 
-   try
-   {
+	try {
 
-      SceneEvent SceneEV(&theScene, gm);
+		SceneEvent SceneEV(&theScene, gm);
 
-      EventHandler Events;
-      Interface* pInterface = new Interface(Events, gm);
-      MainEvent mev;
+		EventHandler Events;
+		Interface* pInterface = new Interface(Events, gm);
+		MainEvent mev;
 
-      Events.Add (cam);
-      Events.Add(&SceneEV);
-      Events.Add (&mev);
-      Events.Add (pInterface);
+		Events.Add (cam);
+		Events.Add(&SceneEV);
+		Events.Add (&mev);
+		Events.Add (pInterface);
 
-      // Load the test Park
-      SceneEV.OnOpen("data/xml/Park.xml");
-      //
+		// Load the test Park
+		SceneEV.OnOpen("data/xml/Park.xml");
+		//
 
-      //////////////////////////////////////////////////////////////
-      //
-      //  Minimum time to display Splash Screen
-      //
-      while (SplashScreenClock.GetElapsedTime() < 2.5)
-      {
-        // display the splash screen for a minumum amount of time (eg 2.5 seconds)
-        // as game initialization time increases due to development, this will likely become redundant.
-      }
-      SplashScreen.Close(); // Done with the splash screen - close it up and forget it
-      //
-      //////////////////////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////
+		//
+		//  Minimum time to display Splash Screen
+		//
+		while (SplashScreenClock.GetElapsedTime() < 2.5) {
+			// display the splash screen for a minumum amount of time (eg 2.5 seconds)
+			// as game initialization time increases due to development, this will likely become redundant.
+		}
+		SplashScreen.Close(); // Done with the splash screen - close it up and forget it
+		//
+		//////////////////////////////////////////////////////////////
 
 
 
-      std::cout << "Starting event loop...\n";
-      int cycle = 0;
-      sf::Window* pWindow = cam->GetWindow();
-      while (mev.IsRunning())
-      {
-         // This should be here so we can create custom events later,
-         // and also so we can create fake events for debugging.
-         sf::Event Event;
-         while (pWindow->GetEvent(Event))
-         {
-            Events.HandleEvents (Event);
-         }
-         // drawing scope
-         cam->Clear ();
-         cam->SetActive();
-         cam->Integrate (60);
-         cam->DrawModel(&theScene); // the background and Park(s).
-         cam->DrawInterface( pInterface );
-         cam->Display ();
+		std::cout << "Starting event loop...\n";
+		int cycle = 0;
+		sf::Window* pWindow = cam->GetWindow();
+		while (mev.IsRunning()) {
+			// This should be here so we can create custom events later,
+			// and also so we can create fake events for debugging.
+			sf::Event Event;
+			while (pWindow->GetEvent(Event)) {
+				Events.HandleEvents (Event);
+			}
+			// drawing scope
+			cam->Clear ();
+			cam->SetActive();
+			cam->Integrate (60);
+			cam->DrawModel(&theScene); // the background and Park(s).
+			cam->DrawInterface( pInterface );
+			cam->Display ();
 
-         // end drawing scope
+			// end drawing scope
 
-         // update scope, thread candidates
-         switch( cycle++ )
-         {
-         case 0:
-            theScene.Update (0, pInterface->GetTimeOfDay());
-            break;
-         case 4:
-            pInterface->GetStats().SetPopulation( thePark.GetPopulation() );
-            pInterface->Update(10);
-            break;
-         case 8:
-            thePark.Update (5, pInterface->GetTimeOfDay());
-            break;
-         case 1:
-         case 2:
-         case 3:
-         case 5:
-         case 6:
-         case 7:
-            break;
+			// update scope, thread candidates
+			switch( cycle++ ) {
+				case 0:
+					theScene.Update (0, pInterface->GetTimeOfDay());
+					break;
+				case 4:
+					pInterface->GetStats().SetPopulation( thePark.GetPopulation() );
+					pInterface->Update(10);
+					break;
+				case 8:
+					thePark.Update (5, pInterface->GetTimeOfDay());
+					break;
+				case 1:
+				case 2:
+				case 3:
+				case 5:
+				case 6:
+				case 7:
+					break;
 
-         default:
-            static int cc_count = 30; //only once in a while
-            if( cc_count < 1 )
-            {
-               People.Update( 100,pInterface->GetTimeOfDay() );
-               cc_count = 20;
-            }
-            cc_count--;
-            cycle = 0;
-            break;
-         }
-         // end update scope
-      }
-      std::cout << mev.IsRunning() << "\n";
-   }
-   catch ( GameException* ex )
-   {
-      std::cout << "Exception caught in main: " << ex->get_Message();
-      std::cout << "Hit a key to close!";
-      char t[4];
-      std::cin.get(t[0]);
-   }
-   return 0;
+				default:
+					static int cc_count = 30; //only once in a while
+					if( cc_count < 1 ) {
+						People.Update( 100,pInterface->GetTimeOfDay() );
+						cc_count = 20;
+					}
+					cc_count--;
+					cycle = 0;
+					break;
+			}
+			// end update scope
+		}
+		std::cout << mev.IsRunning() << "\n";
+	} catch ( GameException* ex ) {
+		std::cout << "Exception caught in main: " << ex->get_Message();
+		std::cout << "Hit a key to close!";
+		char t[4];
+		std::cin.get(t[0]);
+	}
+	return 0;
 }
